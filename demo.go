9d123edd0994938b2310e20e51625aed35c260f3
Forb Yuan <f0rb@163.com>
2024-10-13 16:38:37 +0800

    echo integration sample

--- demo.go
+++ demo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/doytowin/goooqo/core"
 	"github.com/doytowin/goooqo/rdb"
 	"github.com/doytowin/goooqo/web"
+	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
 	log "github.com/sirupsen/logrus"
 	"net/http"
@@ -16,10 +18,61 @@ func main() {
 	tm := rdb.NewTransactionManager(db)
 
 	userDataAccess := rdb.NewTxDataAccess[UserEntity](tm)
-	web.BuildRestService[UserEntity, UserQuery]("/user/", userDataAccess)
 
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	e := echo.New()
+	buildEchoService[UserEntity, UserQuery](e, "/user/", userDataAccess)
+	e.Logger.Fatal(e.Start(":9090"))
+}
+
+func buildEchoService[E core.Entity, Q core.Query](e *echo.Echo,
+	prefix string, dataAccess core.TxDataAccess[E], m ...echo.MiddlewareFunc) {
+	idPrefix := prefix + ":id"
+	e.GET(idPrefix, func(c echo.Context) error {
+		id := c.Param("id")
+		entity, err := dataAccess.Get(c.Request().Context(), id)
+		return writeResult(c, err, entity)
+	}, m...)
+	e.GET(prefix, func(c echo.Context) error {
+		query := *new(Q)
+		web.ResolveQuery(c.QueryParams(), &query)
+		data, err := dataAccess.Page(c.Request().Context(), query)
+		return writeResult(c, err, data)
+	}, m...)
+	e.PATCH(idPrefix, func(c echo.Context) error {
+		entity := *new(E)
+		if err := c.Bind(&entity); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		entity.SetId(&entity, c.Param("id"))
+		data, err := dataAccess.Patch(c.Request().Context(), entity)
+		return writeResult(c, err, data)
+	}, m...)
+	e.PATCH(prefix, func(c echo.Context) error {
+		query := *new(Q)
+		web.ResolveQuery(c.QueryParams(), &query)
+		entity := *new(E)
+		if err := c.Bind(&entity); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		data, err := dataAccess.PatchByQuery(c.Request().Context(), entity, query)
+		return writeResult(c, err, data)
+	}, m...)
+	e.POST(prefix, func(c echo.Context) error {
+		var entities []E
+		if err := c.Bind(&entities); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		data, err := dataAccess.CreateMulti(c.Request().Context(), entities)
+		return writeResult(c, err, data)
+	}, m...)
+	e.DELETE(idPrefix, func(c echo.Context) error {
+		id := c.Param("id")
+		data, err := dataAccess.Delete(c.Request().Context(), id)
+		return writeResult(c, err, data)
+	}, m...)
+}
+
+func writeResult(c echo.Context, err error, data any) error {
+	response := core.Response{Data: data, Success: core.NoError(err), Error: core.ReadError(err)}
+	return c.JSON(0, response)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/doytowin/goooqo/core v0.2.2
 	github.com/doytowin/goooqo/rdb v0.2.2
 	github.com/doytowin/goooqo/web v0.2.2
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/sirupsen/logrus v1.9.3
 )
@@ -14,6 +15,13 @@ require github.com/joho/godotenv v1.5.1 // indirect
 
 require (
 	github.com/doytowin/goooqo/test v0.2.2 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 )
